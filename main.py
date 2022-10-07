3831843e857a665e8fc3a1e5de2f082d98668915
tien thuan <tienthuan05082002@gmail.com>
2022-10-07 23:15:40 +0700

    update texttranlate.kv ui

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
 from kivymd_extensions.akivymd.uix.statusbarcolor import change_statusbar_color
 from kivymd.uix.toolbar import MDTopAppBar
 from screens.mainscreen import MainScreen
+from screens.texttranslate import TextTranslateScreen
 
 
 # clone UI from Google Translate
@@ -23,5 +25,14 @@ class EasyTranslateApp(MDApp):
     def build(self):
         return WindowManager()
 
+    def on_start(self):
+        Window.bind(on_keyboard=self.events)
+
+    def events(self, window, key, *args):
+        if key == 27:
+            if self.root.current == '_main_screen_':
+                return False
+            self.root.current = self.root.previous()
+            return True
 
 EasyTranslateApp().run()
---
+++ screens/texttranslate.py
@@ -0,0 +1,8 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+Builder.load_file('screens/texttranslate.kv')
+
+
+class TextTranslateScreen(Screen):
+    pass
