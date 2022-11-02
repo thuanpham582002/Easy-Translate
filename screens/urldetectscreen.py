faff71ddc779b04b22d6fdaf2e30f145ae5f7082
Codersaurus <huudatvu6139@gmail.com>
2022-11-03 00:13:11 +0700

    Update UI in urldetectscreen.kv

--- screens/urldetectscreen.py
+++ screens/urldetectscreen.py
@@ -1,6 +1,6 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
-from kivymd.uix.list import OneLineListItem
+from kivymd.uix.list import OneLineListItem, BaseListItem
 
 import screens.constant
 
@@ -12,6 +12,7 @@ class UrlDetectScreen(Screen):
     def on_pre_enter(self, *args):
         self.ids.url_detect_list.clear_widgets()
         for x in screens.constant.list_url_detect:
+
             self.ids.url_detect_list.add_widget(
                 OneLineListItem(text=x, on_release=lambda this: update(this)))
         return super().on_pre_enter(*args)
@@ -25,7 +26,9 @@ class UrlDetectScreen(Screen):
 def url_translate(url):
     if screens.constant.source_language == 'Detect language' == screens.constant.destination_language:
         return url
-    return 'http://translate.google.com/translate?js=n&sl=' + screens.constant.list_language_dict[screens.constant.source_language] + '&tl=' + screens.constant.list_language_dict[screens.constant.destination_language] + '&u=' + url
+    return 'http://translate.google.com/translate?js=n&sl=' \
+           + screens.constant.list_language_dict[screens.constant.source_language] \
+           + '&tl=' + screens.constant.list_language_dict[screens.constant.destination_language] + '&u=' + url
 
 
 def update(x: OneLineListItem):
